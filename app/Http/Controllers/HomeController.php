9146d4cdb568a452d9569ad5b03ffe99037bbd54
stevy-sys <62965624+stevy-sys@users.noreply.github.com>
2021-02-09 21:55:38 +0100

    suppression du home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,8 +22,4 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
-    }
 }
